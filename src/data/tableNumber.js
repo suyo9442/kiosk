5ee367db08dcc4e9ef59a8d9654c680bfc020494
kimsoo <72788139+suyo9442@users.noreply.github.com>
2022-12-06 14:32:47 +0900

    setting

---
+++ src/data/tableNumber.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    list: 1,
+  },
+  {
+    list: 2,
+  },
+  {
+    list: 3,
+  },
+  {
+    list: 4,
+  },
+  {
+    list: 5,
+  },
+  {
+    list: 6,
+  },
+  {
+    list: 7,
+  },
+  {
+    list: 8,
+  },
+  {
+    list: 9,
+  },
+  {
+    list: 10,
+  },
+];
